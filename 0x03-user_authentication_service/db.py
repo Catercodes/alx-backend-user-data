aeecc74a07b4c98999f2a937c0ca9ed2f1965354
Catercodes <chrisadamsadewuni@gmail.com>
2024-07-19 15:36:19 +0100

    Creating user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -34,6 +34,7 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """The method saves a user to the database
         """
+        new_user = User()
         # Create a new User object
         new_user = User(email=email, hashed_password=hashed_password)
 
